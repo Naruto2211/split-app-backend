d924ccb721fed0efc535caf2254287198d37eb58
Devraj2211 <127536394+Naruto2211@users.noreply.github.com>
2025-06-17 03:25:41 +0530

    Update expenseRoutes.js

--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const { addExpense, getExpenses, updateExpense, deleteExpense } = require('../controllers/expenseController');
 const router = express.Router();
+const {
+    addExpense,
+    getAllExpenses,
+    updateExpense,
+    deleteExpense,
+    getSettlements,
+    getPeople,
+    getBalances
+} = require('../controllers/expenseController');
 
-router.post('/', addExpense);
-router.get('/', getExpenses);
-router.put('/:id', updateExpense);
-router.delete('/:id', deleteExpense);
+// Expense Management Routes
+router.post('/expenses', addExpense);
+router.get('/expenses', getAllExpenses);
+router.put('/expenses/:id', updateExpense);
+router.delete('/expenses/:id', deleteExpense);
+
+// Settlement Calculation Route
+router.get('/settlements', getSettlements);
+
+// New Routes for Balances & People (Added as per assignment)
+router.get('/people', getPeople);
+router.get('/balances', getBalances);
 
 module.exports = router;
